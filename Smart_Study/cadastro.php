a632caacddc2374e1437a69ecef2644d5640e893
AGomes002 <151149115+AGomes002@users.noreply.github.com>
2024-11-12 07:31:33 -0300

    Update cadastro.php

--- Smart_Study/cadastro.php
+++ Smart_Study/cadastro.php
@@ -1,62 +1,23 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>replit</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-  <?php
+<?php
 
 $dbhost = 'localhost';
 $dbUserName = 'root';
 $dbPassword = '';
-$dbName = 'smart_study';
+$dbName = '';
 
 $conexao = new mysql ($dbhost, $dbUserName, $dbPassword, $dbName)
-    $posicao = null;
-    $usuarios = [
-      [
-        'email' => 'jales@email.com',
-        'senha' => '123456',
-        'nome' => 'Jales',
-        'idade' => '31',
-      ],
-      [
-        'email' => 'alana@email.com',
-        'senha' => '123456',
-        'nome' => 'Alana',
-        'idade' => '29',
-      ]
-    ];
-  ?>
-  
-  <!-- PROCESSAMENTO -->
-  
-  <div class="row">
-    <div class="col-md-12 bg">
-        <h1>Perfil</h1>
-        
-        <fieldset>
 
-          <legend>Informações Básicas</legend>
+//if ($conexao -> conect_errno)
+//{
+    //echo "Erro";
+//}
+//else{
+  //  echo "Conexão efetuada";
+//}
+if (isset($_POST["submit"]))
+$nome = ($_POST['nome']);
+$idade = ($_POST['idade']);
+$telefone = ($_POST['telefone']);
+$email = ($_POST['email']);
 
-          <!-- EXIBIÇÃO -->
-
-          <label><b>Nome:</b></label>
-          <!-- exibir nome -->
-
-          <label><b>Idade:</b></label>
-          <!-- exibir idade -->
-
-        </fieldset>
-
-        <a href="cadastro.html">Login</a>
-
-    </form>
-    </div>
-  </div>
-</body>
-</html>
+?>
