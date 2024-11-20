db029b133d21b0608ed33b64427b15f8f6ef40ae
euyeu <151038619+euyeu@users.noreply.github.com>
2024-11-20 17:16:15 -0300

    Create mensagens.js

---
+++ Smart_Study/mensagens.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    exibirMensagens();
+});
+
+function exibirMensagens() {
+    var mensagens = JSON.parse(localStorage.getItem("mensagens")) || [];
+    var mensagensList = document.getElementById("mensagensList");
+
+    if (mensagens.length === 0) {
+        mensagensList.innerHTML = "<li>Nenhuma mensagem encontrada.</li>";
+    } else {
+        mensagensList.innerHTML = mensagens
+            .map(
+                (mensagem) => `
+                <li>
+                    <strong>Nome:</strong> ${mensagem.nome}<br>
+                    <strong>E-mail:</strong> ${mensagem.email}<br>
+                    <strong>Mensagem:</strong> ${mensagem.mensagem}
+                </li>`
+            )
+            .join("");
+    }
+}
